eedb20e33bbe320eb83affcd90ef6fe3c9e1ed01
Maninder <maninder.bhogal@hotmail.com>
2021-04-12 11:41:40 -0400

    update fetch_distinctions_people.py

--- donnees/fetch_distinctions_people.py
+++ donnees/fetch_distinctions_people.py
@@ -1,6 +1,5 @@
 """Script qui extrait les distinctions (avec la date) obtenues par les
 personnes qui ont occupé une fonction dans une oeuvre réalisée au Québec.
-
 Malheureusement, on ne connaît pas dans quelle oeuvre est associée la distinction.
 """
 
@@ -21,13 +20,16 @@ def get_distinctions(person_result):
     PREFIX p: <http://www.wikidata.org/prop/>
     PREFIX ps: <http://www.wikidata.org/prop/statement/>
     PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
-    SELECT DISTINCT ?distinction ?date WHERE {{
+    SELECT DISTINCT ?distinction ?date ?distinctionLabel WHERE {{
         <{person_wikidata}> p:P166 [ ps:P166 ?distinction ; pq:P585 ?date ] .
+        ?distinction  rdfs:label ?distinctionLabel . 
+	    FILTER (lang(?distinctionLabel) = "fr")
+
     }}
     """
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
-        return [(r['distinction']['value'], r['date']['value']) for r in sparql.query().convert()['results']['bindings']]
+        return [(r['distinction']['value'], r['date']['value'], r['distinctionLabel']['value']) for r in sparql.query().convert()['results']['bindings']]
     except:
         return []
 
@@ -42,13 +44,15 @@ PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX frbroo: <http://iflastandards.info/ns/fr/frbr/frbroo/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT DISTINCT ?person ?personWikidata ?distinction WHERE {
+SELECT DISTINCT ?person ?personWikidata ?personLabel ?distinction WHERE {
   ?recordingEvent crm:P9_consists_of ?recordingActivity .
   ?recordingEvent crm:P7_took_place_at <http://data.cinematheque.qc.ca/resource/Place216> .
   ?recordingEventCarriedOutBy crm:P01_has_domain ?recordingActivity .
   ?recordingEventCarriedOutBy crm:P02_has_range ?person .
   ?person a crm:E21_Person .
   ?person owl:sameAs ?personWikidata .
+  ?person rdfs:label ?personLabel . 
+
 }
 """
     sparql.setQuery(query)
@@ -61,16 +65,15 @@ SELECT DISTINCT ?person ?personWikidata ?distinction WHERE {
     distinctions_people = zip(results, pool.map(get_distinctions, results))
     for person_result, distinctions in distinctions_people:
         for distinction in distinctions:
-            dict_data.append({ 'person': person_result['person']['value'], 'distinction': distinction[0], 'date': distinction[1] })
+            dict_data.append({ 'person': person_result['person']['value'], 'personLabel': person_result['personLabel']['value'], 'distinction': distinction[0], 'date': distinction[1],'distinctionLabel': distinction[2]})
 
     with open('distinctions_people.csv', 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=['person', 'distinction', 'date'])
+        writer = csv.DictWriter(csvfile, fieldnames=['person', 'personLabel' , 'distinction', 'distinctionLabel', 'date'])
         writer.writeheader()
         for data in dict_data:
             writer.writerow(data)
 
 def main():
-    # fetch_movie_genres()
     fetch_quebec_person_distinction()
 
 if __name__ == '__main__':
